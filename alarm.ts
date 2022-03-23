3709cacbee9e60c427b0d06edea180dc75d07085
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-03-23 22:57:21 +0100

    Aktualizovat main.blocks, main.ts, alarm.ts

--- alarm.ts
+++ alarm.ts
@@ -1,16 +1,8 @@
 //% weight=100 color=#d1242c icon="\uf0f3" block="Alarm"
 namespace alarm {
     let alarm = false
-
-    basic.showLeds(`
-        . . # . .
-        . # # # .
-        . # # # .
-        # # # # #
-        . . . . .
-        `)
-
-        
+    let methodLock = false
+      
     /**
     * Spustí alarm a pošle všem zařízením v okolí pokyn ke spuštění alarmu
     */
@@ -18,13 +10,6 @@ namespace alarm {
 
     export function turnOnAlarmAndBroadcast(): void {
         alarm = true
-        basic.showLeds(`
-        . . # . .
-        . # # # .
-        . # # # .
-        # # # # #
-        . . # . .
-        `)
         radio.sendString("alarm_on")
     }
 
@@ -35,52 +20,53 @@ namespace alarm {
 
     export function turnOffAlarmAndBroadcast(): void {
         alarm = false
-        basic.showLeds(`
-        . . # . .
-        . # # # .
-        . # # # .
-        # # # # #
-        . . . . .
-        `)
         radio.sendString("alarm_off")
     }
 
     /**
-    * Přijme pokyn od jiného zařízení
+    * Přijme pokyn od jiného zařízení, vrací true/false podle toho, jestli je alarm vypnutý nebo zapnutý
     * @message Pokyn
     */
     //% block="Přijmout pokyn %message"
 
-    export function receiveBroadcast(message: string): void {
+    export function receiveBroadcast(message: string): boolean {
         if (message == "alarm_on" && alarm == false) {
             alarm = true
-            basic.showLeds(`
-            . . # . .
-            . # # # .
-            . # # # .
-            # # # # #
-            . . # . .
-            `)
             radio.sendString("alarm_on")
         } else if (message == "alarm_off" && alarm == true) {
             alarm = false
-            basic.showLeds(`
-            . . # . .
-            . # # # .
-            . # # # .
-            # # # # #
-            . . . . .
-            `)
             radio.sendString("alarm_off")
         }
+        return alarm
     }
 
-    basic.forever(function () {
-        if (alarm) {
-            music.playTone(262, music.beat(BeatFraction.Whole))
-            music.playTone(294, music.beat(BeatFraction.Whole))
-        }
-    })
+
+
+    /**
+    * Zkontroluje, jestli nedošlo k pohybu
+    * @action Příkazy, které se provedou při moc zapnutí alarmu
+    */
+    //% block="Při zapnutí alarmu"
+    export function onGuardAwaken(action: () => void) {
+        const eventID = 111 + Math.randomRange(0, 100);
+
+        control.onEvent(eventID, 0, function () {
+            control.inBackground(() => {
+                methodLock = true
+                action()
+                methodLock = false
+            })
+        })
+
+        control.inBackground(() => {
+            while (true) {
+                if (alarm) {
+                    action()
+                }
+                basic.pause(20)
+            }
+        })
+    }
 
 
 
--- main.ts
+++ main.ts
@@ -1,10 +1,52 @@
+alarm.onGuardAwaken(function () {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
 input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . # # # .
+        # # # # #
+        . . # . .
+        `)
     alarm.turnOnAlarmAndBroadcast()
 })
 radio.onReceivedString(function (receivedString) {
-    alarm.receiveBroadcast(receivedString)
+    vysledek = alarm.receiveBroadcast(receivedString)
+    if (vysledek == true) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            . # # # .
+            # # # # #
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            . # # # .
+            # # # # #
+            . . . . .
+            `)
+    }
 })
 input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . # # # .
+        # # # # #
+        . . . . .
+        `)
     alarm.turnOffAlarmAndBroadcast()
 })
+let vysledek = false
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    . # # # .
+    # # # # #
+    . . . . .
+    `)
 radio.setGroup(1)
